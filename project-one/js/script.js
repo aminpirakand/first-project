f27645d4fba27667e04fd9e9f4acceb82a788639
cclxxxix <48237328+cclxxxix@users.noreply.github.com>
2019-09-26 00:53:28 +0330

    upload project

---
+++ project-one/js/script.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    var x = document.getElementById("search-id");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myLog() {
+    window.open('login.html');
+}
+
+function myReg() {
+    window.open('register.html');
+
+}
+
+function Close() {
+    window.close("");
+}
+
+function myShop() {
+    window.open('products.html');
+}
